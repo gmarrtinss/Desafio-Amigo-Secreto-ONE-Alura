24d3f1d91a0426c7bda54aee072563907a3e2292
Guilherme Martins <97321294+gmarrtinss@users.noreply.github.com>
2025-03-14 22:05:34 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 // 1. Criar um array para armazenar os nomes
 const amigos = [];
 
